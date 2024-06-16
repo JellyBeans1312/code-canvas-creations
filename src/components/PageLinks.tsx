66df1a0d9ac661025be025b62d6f857e33181ffd
Aidan McKay <aidanmckay2000@gmail.com>
2024-06-16 18:25:41 -0500

    Changed gradient on image

--- src/components/PageLinks.tsx
+++ src/components/PageLinks.tsx
@@ -34,19 +34,19 @@ function PageLink({ page }: { page: Page }) {
         position="left"
         className="relative flex flex-col items-start pl-8"
       >
-        <h3 className="mt-6 text-base font-semibold text-neutral-950">
+        <h3 className="mt-6 text-base font-semibold text-accentOne">
           {page.title}
         </h3>
         <time
           dateTime={page.date}
-          className="order-first text-sm text-neutral-600"
+          className="order-first text-sm text-neutral-300"
         >
           {formatDate(page.date)}
         </time>
-        <p className="mt-2.5 text-base text-neutral-600">{page.description}</p>
+        <p className="mt-2.5 text-base text-neutral-300">{page.description}</p>
         <Link
           href={page.href}
-          className="mt-6 flex gap-x-3 text-base font-semibold text-neutral-950 transition hover:text-neutral-700"
+          className="mt-6 flex gap-x-3 text-base font-semibold text-accentOne transition hover:text-accentTwo"
           aria-label={`Read more: ${page.title}`}
         >
           Read more
@@ -71,14 +71,14 @@ export function PageLinks({
 }) {
   return (
     <div className={clsx('relative pt-24 sm:pt-32 lg:pt-40', className)}>
-      <div className="absolute inset-x-0 top-0 -z-10 h-[884px] overflow-hidden rounded-t-4xl bg-gradient-to-b from-neutral-50 via-accentTwo via-50% to-secondary">
+      <div className="absolute inset-x-0 top-0 -z-10 h-[884px] overflow-hidden rounded-t-4xl bg-gradient-to-b from-secondary to-primary">
         <GridPattern
-          className="absolute inset-0 h-full w-full fill-neutral-100 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+          className="absolute inset-0 h-full w-full fill-accentOne stroke-primary/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_0%)]"
           yOffset={-270}
         />
       </div>
 
-      <SectionIntro title={title} invert smaller>
+      <SectionIntro title={title} invert={false} smaller>
         {intro && <p>{intro}</p>}
       </SectionIntro>
 
