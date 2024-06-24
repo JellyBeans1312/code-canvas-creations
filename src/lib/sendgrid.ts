244977578bd3869e6754d8bb76c5eddee821555a
Aidan McKay <aidanmckay2000@gmail.com>
2024-06-24 11:44:09 -0500

    More comments

--- src/lib/sendgrid.ts
+++ src/lib/sendgrid.ts
@@ -1,5 +1,5 @@
-import * as sgMail from '@sendgrid/mail'
-const sgApiKey = process.env.SENDGRID_API_KEY
+// import * as sgMail from '@sendgrid/mail'
+// const sgApiKey = process.env.SENDGRID_API_KEY
 // sgMail.setApiKey(sgApiKey)
  type requestObj = {
     to: string,
